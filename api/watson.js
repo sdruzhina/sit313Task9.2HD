fdf24e5a9594d74ab3382f967df1b74e2d26c0c0
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-10-16 09:48:57 +1100

    Saving Watson response

--- api/watson.js
+++ api/watson.js
@@ -1,10 +1,11 @@
 const config = require('../config');
 const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
+const Task = require('../models/Task');
 
 module.exports = {
     // Classify an image with IBM Watson
-    classify(imageUrl) {
+    classify(imageUrl, taskId) {
         const visualRecognition = new VisualRecognitionV3({
         version: '2018-03-19',
         authenticator: new IamAuthenticator({
@@ -21,6 +22,27 @@ module.exports = {
         .then(response => {
             const classifiedImages = response.result;
             console.log(JSON.stringify(classifiedImages, null, 2));
+
+            // Update the task in DB
+            Task.updateOne(
+                { _id: taskId },
+                { 
+                    status: 'COMPLETED',
+                    response: response,
+                    updatedAt: Date.now()
+                },
+                { upsert: false }, function(err, result) {
+                if (err) {
+                    res.json(err);
+                }
+                else {
+                    res.json({ 
+                        status: 'success', 
+                        message: 'Task successfully updated.' 
+                    })
+                }
+            });
+            
         })
         .catch(err => {
             console.log('error:', err);
